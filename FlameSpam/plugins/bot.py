7f8875a9f497f699902a4450dff916345b064544
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2022-06-07 08:24:44 +0530

    Update bot.py

--- FlameSpam/plugins/bot.py
+++ FlameSpam/plugins/bot.py
@@ -93,8 +93,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"▒█▀▀█ ▒█▀▀▀█ ▀▀█▀▀\n▒█▀▀▄ ▒█░░▒█ ░▒█░░\n▒█▄▄█ ▒█▄▄▄█ ░▒█░░\n\nϟ
-        ғʟᴀᴍᴇ sᴘᴀᴍ ϟ︎ `{ms}` ᴍs")
+        await event.edit(f"ғʟᴀᴍᴇ sᴘᴀᴍ  `{ms}` ᴍs")
         
         
 
